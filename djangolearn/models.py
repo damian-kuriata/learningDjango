1c29d7ded6b30c175870040a7ebbc185af1dac34
damian-kuriata <damian.kuriata2001@gmail.com>
2020-12-26 18:14:02 +0100

    Add help texts for 'Language' and 'Phrase', mark some texts for translation.

--- djangolearn/models.py
+++ djangolearn/models.py
@@ -12,6 +12,8 @@ from django.core.validators import RegexValidator, MinValueValidator, \
 def get_upload_path(instance, filename):
     return os.path.join("phrase_images", instance.user.id, filename)
 
+# Todo implement 'get_absolute_url' for 'User' class
+
 
 class Language(models.Model):
     LANG_ENGLISH = "en"
@@ -26,7 +28,7 @@ class Language(models.Model):
         (LANG_SPANISH, _("Spanish")),
         (LANG_POLISH, _("Polish"))
     )
-    name = models.CharField(max_length=20,
+    name = models.CharField(_("name"), max_length=20,
                             validators=[RegexValidator("^[A-Z ]+$",
                                         flags=re.I)],
                             choices=LANG_CHOICES)
@@ -36,21 +38,33 @@ class Language(models.Model):
         verbose_name = _("language")
         verbose_name_plural = _("languages")
 
+    # TODO: implement 'get_absolute_url' method
+
 
 class Phrase(models.Model):
-    non_translated_text = models.TextField(_("non_translated_text"))
+    non_translated_text = models.TextField(_("non_translated_text"),
+                                           help_text=_("Text you will translate"))
     translated_text = models.TextField(_("translated_text"))
     language = models.ForeignKey(Language, models.CASCADE)
-    user = models.ForeignKey(User, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE,
+                             help_text=_("User who added a language"))
     # Number in range 0 to 10 indicating possibility of this phrase being selected
     priority = models.IntegerField(_("priority"), validators=[
-        MinValueValidator(0), MaxValueValidator(10)])
-    image = models.ImageField(_("image"), upload_to=get_upload_path, blank=True)
+        MinValueValidator(0), MaxValueValidator(10)], help_text=_("Number in range"
+                                                                " 0 to 10 "
+                                                                "indicating "
+                                                                "possibility of "
+                                                                "this phrase "
+                                                                "being selected"))
+    image = models.ImageField(_("image"), upload_to=get_upload_path, blank=True,
+                              help_text=_("[optional] Image you associated "
+                                          "with phrase"))
 
     class Meta:
         ordering = ["language"]
         verbose_name = _("phrase")
         verbose_name_plural = _("phrases")
+    # Todo: implement 'get_absolute_url' method
 
 
 @receiver(pre_delete, sender=Phrase)
